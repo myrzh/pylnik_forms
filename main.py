3d2e9355c7795d4b9af119e885d33f45b857db45
Ivan Shtarev <ishtarev@outlook.com>
2023-11-01 00:22:13 +0300

    put everything into functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import zipfile
 import csv
 
-from pprint import pprint
 from docx import Document
 from docx.shared import Pt
 
@@ -9,53 +8,65 @@ from docx.shared import Pt
 GROUP_NUMBER = "5151003/30002"
 
 
-def main():
-    # practice_number = input("Enter practice number: ")
-    practice_number = "TEMPLATE"
-
+def extract_csv(practice_number: str) -> None:
     with zipfile.ZipFile(f"ORG_{practice_number}.csv.zip", "r") as zip_ref:
         zip_ref.extractall("responses_extracted")
 
-    with open(
-        f"responses_extracted/ORG_{practice_number}.csv", encoding="utf8"
-    ) as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
-        records = list(reader)
 
-    responses = records.copy()
-    # for record in records:
-    #     if responses:
-    #         abnormal_exit = False
-    #         for index, item in enumerate(responses):
-    #             if item["Имя"] == record["Имя"]:
-    #                 print("YASS")
-    #                 responses[index] == record
-    #                 abnormal_exit = True
-    #                 break
-    #         if not abnormal_exit:
-    #             responses.append(record)
-    #     else:
-    #         responses.append(record)
+def actualize_records(records: list) -> list:
+    used_names = []
+    responses = []
 
-    # pprint(responses)
+    for record in records[::-1]:
+        if record["Имя"] not in used_names:
+            responses.append(record)
+            used_names.append(record["Имя"])
+    responses.reverse()
 
+    return responses
+
+
+def init_document(practice_number: str) -> Document:
     document = Document()
-
-    document.add_paragraph(f"Практика {practice_number}, группа {GROUP_NUMBER}")
-    document.add_paragraph("\n")
 
     style = document.styles["Normal"]
     font = style.font
     font.name = "Times New Roman"
     font.size = Pt(12)
 
+    document.add_paragraph(f"Практика {practice_number}, группа {GROUP_NUMBER}\n")
+
+    return document
+
+
+def write_response(document: Document, response: dict) -> Document:
+    document.add_paragraph(response["Имя"])
+
+    for index in range(2, 4):
+        document.add_paragraph(
+            list(response.keys())[index].replace(" без заголовка", "")
+        )
+        document.add_paragraph(list(response.values())[index])
+
+    document.add_page_break()
+
+    return document
+
+
+def main():
+    # practice_number = input("Enter practice number: ")
+    practice_number = "TEMPLATE"
+
+    with open(
+        f"responses_extracted/ORG_{practice_number}.csv", encoding="utf8"
+    ) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
+        responses = actualize_records(list(reader))
+
+    document = init_document(practice_number)
+
     for response in responses:
-        document.add_paragraph(response["Имя"])
-        document.add_paragraph(list(response.keys())[2].replace(" без заголовка", ""))
-        document.add_paragraph(list(response.values())[2])
-        document.add_paragraph(list(response.keys())[3].replace(" без заголовка", ""))
-        document.add_paragraph(list(response.values())[3])
-        document.add_page_break()
+        write_response(document, response)
 
     document.save(f"Практика {practice_number}.docx")
 
